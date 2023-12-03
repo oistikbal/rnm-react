27a04ff1335c2c531f3e472afaddd05861ba6034
Omer Faruk Istikbal <ofarukistikbal@hotmail.com>
2023-12-03 13:55:22 +0300

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from "react";
+import CharacterCard from "./components/CharacterCard";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css"
+
+
+export default function App() {
+  return (
+    <CharacterCard></CharacterCard>
+  );
+}
---
+++ src/components/CharacterCard.jsx
@@ -0,0 +1,20 @@
+import Card from 'react-bootstrap/Card';
+
+export default function CharacterCard({Name = "Name",}) {
+  return (
+    <Card style={{ width: '15rem' }}>
+      <Card.Img variant="top" src="https://rickandmortyapi.com/api/character/avatar/690.jpeg" />
+      <Card.Body>
+        <Card.Title>{Name}</Card.Title>
+        <Card.Text>
+        <div>
+            <p>Location</p>
+        </div>
+        <div>
+            <p>First</p>
+        </div>
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+}
